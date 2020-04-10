c169148bae36173f7e111f3ebeac3d39b885d30a
woohwan oh <khbohwan@naver.com>
2020-04-10 22:40:05 +0900

    Create gugudan.py

---
+++ gugudan.py
@@ -0,0 +1,14 @@
+#파이썬 구구단 (gugudan.py )
+
+print(f'오진희  ---  구구단 출력')
+for i in range(1,10,1):
+
+    print('*' * 30)
+    print(f'{i} 단')
+    print('*'*30)
+    for j in range(1,10,1):
+        print(f'{i} x {j} = {i*j}')
+    print('\n')
+print('*' * 50)
+
+# 수정 : 2020-04-10
